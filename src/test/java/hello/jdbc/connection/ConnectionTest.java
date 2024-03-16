db7ba47403ed2e73f4ae982e5fbd5d3927299e19
Yuhwan Kim <yhkim8046@gmail.com>
2024-03-17 02:22:53 +1300

    Connection pool and datasource started

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,40 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.PASSWORD;
+import static hello.jdbc.connection.ConnectionConst.USERNAME;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection connection1 = DriverManager.getConnection(ConnectionConst.URL, USERNAME, PASSWORD);
+        Connection connection2 = DriverManager.getConnection(ConnectionConst.URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}",connection1, connection1.getClass());
+        log.info("connection={}, class={}",connection2, connection2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(ConnectionConst.URL,USERNAME,PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException{
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}",con1, con1.getClass());
+        log.info("connection={}, class={}",con2, con2.getClass());
+    }
+
+}
